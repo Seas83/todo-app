f497398da6035e5e09dd8cebdf83a3742c57c729
Seas83 <alkotifanf@gmail.com>
2026-07-28 12:18:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -187,8 +187,8 @@ else:
     with st.expander("📌  الأوامر والتعليمات", expanded=False):
         if is_admin:
             with st.form("update_instructions_form"):
-                updated_text = st.text_area( value=current_instructions, height=120)
-                update_btn = st.form_submit_button("حفظ وتحديث التعليمات")
+                     updated_text = st.text_area("تعديل التعليمات :", value=current_instructions, height=120)
+                     update_btn = st.form_submit_button(،"حفظ وتحديث التعليمات")
                 if update_btn:
                     try:
                         check_exist = supabase.table("settings").select("*").eq("key", "instructions").execute()
